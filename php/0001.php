202130a6dc09ad337b7ef791b763a16cd0706e72
geo <geo@endurant.dev>
2021-03-27 15:14:51 -0400

    added q

--- php/0001.php
+++ php/0001.php
@@ -1,8 +1,4 @@
 <?php
-/*
- * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. 
- * Find the sum of all the multiples of 3 or 5 below 1000.
- */
 $sum = 0;
 
 for($i = 1; $i < 1000; $i++)
@@ -13,3 +9,7 @@ for($i = 1; $i < 1000; $i++)
 
 print $sum;
 ?>
+
+
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. 
+//Find the sum of all the multiples of 3 or 5 below 1000.
